fe82bb0ffe7cb86fa3814cd38f504672fc6ddc23
Angel Torres <angeltorres.dev@gmail.com>
2020-06-30 13:36:43 -0500

    added some types and resolvers

--- server/server.js
+++ server/server.js
@@ -1,14 +1,78 @@
 const { ApolloServer, gql } = require('apollo-server');
 
+const authors = [
+    {
+        id: 0,
+        name: "George",
+        email: "geroge@mail.com"
+    },
+    {
+        id: 1,
+        name: "Sam",
+        email: "sam@mail.com"
+    },
+    {
+        id: 2,
+        name: "Estella",
+        email: "estella@mail.com"
+    },
+]
+
+const books = [
+    {
+        id: 0,
+        name: "Some Cool Title",
+        authorId: 0
+    },
+    {
+        id: 1,
+        name: "Title of Cool Book",
+        authorId: 0
+    },
+    {
+        id: 2,
+        name: "Another Cool Book",
+        authorId: 1
+    },
+    {
+        id: 3,
+        name: "Some Book",
+        authorId: 1
+    },
+    {
+        id: 4,
+        name: "Thinking Of A Title",
+        authorId: 2
+    },
+    {
+        id: 5,
+        name: "Another Cool Title",
+        authorId: 2
+    },
+]
+
 const typeDefs = gql`
     type Query {
-        greeting: String
+        authors: [Author]
+    }
+
+    type Author {
+        id: Int!
+        name: String
+        email: String
+        books: [Book]
+    }
+
+    type Book {
+        id: Int!
+        name: String
+        author: String
     }
 `;
 
 const resolvers = {
     Query: {
-        greeting: () => 'Hello GraphQL world!'
+        authors: () => authors
     }
 }
 
